174f0d2cc0f4700c6c0d3f9d383cc489555b21e7
FerPeke <ferna9752@gmail.com>
2023-05-05 23:40:24 -0500

    No funciona

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const titulo =  document.querySelector('#titulo');
+const btn_mostrar = document.querySelector("#mostrar_titulo");
+
+const mostrar_mensaje = (mensaje) =>{
+    titulo.innerHTML = mensaje;
+}
+
+btn_mostrar.addEventListener(
+    'click', mostrar_mensaje("Vamos a mimir amigos")
+);
