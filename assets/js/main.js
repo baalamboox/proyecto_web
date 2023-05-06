b127a0eefe9ea881e8ee04e9f20348212847dd31
FerPeke <ferna9752@gmail.com>
2023-05-05 23:58:53 -0500

    Modificacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
-const titulo =  document.querySelector('#titulo');
-const btn_mostrar = document.querySelector("#mostrar_titulo");
-
+const titulo =  document?.querySelector('#titulo');
+const btn_mostrar = document?.querySelector("#btn_mostrar");
 const mostrar_mensaje = (mensaje) =>{
     titulo.innerHTML = mensaje;
 }
-
 btn_mostrar.addEventListener(
     'click', mostrar_mensaje("Vamos a mimir amigos")
 );
